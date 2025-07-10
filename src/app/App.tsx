1085c876abe84d42590958565415bc0dad4c11e4
BigBoyTimur <roadyleet@gmail.com>
2025-07-10 17:08:57 +0300

    feat(1 модуль): Настроена базовая конфигурация проекта

---
+++ src/app/App.tsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { Suspense } from "react"
+
+import { useTheme } from "./providers/ThemeProvider/lib/useTheme"
+import { classNames } from "shared/lib/classNames/classNames"
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage"
+
+import './styles/index.scss'
+
+export const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
+    return (
+        <div className={classNames('app', {}, [ theme ])}>
+            <button onClick={ toggleTheme }>TOGGLE</button>
+            <Link to='/'>Главная</Link>
+            <Link to='/about'>О сайте</Link>
+            <Suspense fallback={ <div>Loading...</div> }>
+                <Routes>
+                        <Route path='/' element={<MainPage /> } />
+                        <Route path='/about' element={ <AboutPage /> } />
+                </Routes>
+            </Suspense>
+        </div>
+    )
+}
